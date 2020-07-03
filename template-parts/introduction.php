e1d7f4eab3038cd9c26200cdcbed51918a00eae8
Ash - SIlverless <ash@silverless.co.uk>
2020-07-03 10:59:40 +0100

    FIX - conditional read more

--- template-parts/introduction.php
+++ template-parts/introduction.php
@@ -2,10 +2,12 @@
     <div class="introduction">
         <h3 class="heading heading__5 heading__accent"><?php the_field('heading');?></h3>
         <p><?php the_field('introduction_copy');?></p>
-        <div class="read-more-wrapper">
-        <p><?php the_field('introduction_read_more_copy');?></p>
-        <a href="" class="button read-more-wrapper--close">Read Less</a>
-        </div>
-        <a href="" class="button read-more--open">Read More</a>
+        <?php if(get_field('introduction_read_more_copy')):?>
+            <div class="read-more-wrapper">
+            <p><?php the_field('introduction_read_more_copy');?></p>
+            <a href="" class="button read-more-wrapper--close">Read Less</a>
+            </div>
+            <a href="" class="button read-more--open">Read More</a>
+        <?php endif;?>
     </div>
 </div>
